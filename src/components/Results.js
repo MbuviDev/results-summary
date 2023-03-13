5f3eeaea381e997369df2dbd7cf8e579054e9cbb
Meshack <mbuvi.meshack99@gmail.com>
2023-03-13 23:47:32 +0300

    styles

--- src/components/Results.js
+++ src/components/Results.js
@@ -3,10 +3,11 @@ import React from "react";
 function Results () {
     return (
     <div>
-        <h1 className="font-mono text-2xl underline "> Your Result</h1>
+        <h1 className="font-mono text-2xl  font-bold "> Your Result</h1>
         <h2>Great</h2>
-        <p className="">76 </p>
+        <p className="text-3xl font-bold">76 </p>
         <p>of 100</p>
+        <p className="text-2xl font-bold">Great</p>
         <p> You scored higher than 65% of the people who have taken this test</p>
     </div>
     )
--- src/components/Summary.js
+++ src/components/Summary.js
@@ -5,14 +5,14 @@ function Summary () {
     return (
         <div>
         <div>
-            <h1 className="underline text-2xl">Summary </h1>
+            <h1 className="font-bold text-2xl">Summary </h1>
                 <li>Reaction   80/100</li>
                <li>Memory     92/100</li> 
                 <li>Verbal    61/100</li>
                 <li>Virtual   72/100</li>
         </div>
         <div>
-            <button className="bg-blue-200 underline "> Continue </button>
+            <button className="bg-blue-200 font-bold text-center "> Continue </button>
         </div>
         </div>
     )
