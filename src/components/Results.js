1c9117c46c91e3422cdec36ec1abf8e04d4d82e6
Meshack <mbuvi.meshack99@gmail.com>
2023-03-27 17:34:11 +0300

    styles

--- src/components/Results.js
+++ src/components/Results.js
@@ -5,7 +5,7 @@ function Results () {
     <div class="bg-white rounded-md text-center">
         <div class="">
         <h1 className="font-mono text-2xl font-bold underline underline-offset-8 mt-4  "> Your Result</h1>
-         <div class="  mt-2 mb-2 relative absolute left-40 rounded-full bg-sky-200 w-24 h-24 p-2">
+         <div class="  mt-2 mb-2 relative absolute left-80 rounded-full bg-sky-200 w-24 h-24 p-2">
              <p className="text-3xl font-bold mt-4" >76 </p>
              <p>of 100</p>
         </div> 
